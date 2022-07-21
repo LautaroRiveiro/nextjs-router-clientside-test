9f01f3665b88399fd694ce467bd1027f9412cdf0
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2022-07-21 17:59:52 -0300

    log pathname in middleware

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
+  console.log(request.nextUrl.pathname)
+  
   if (request.nextUrl.pathname === '/') {
     if (Math.random() > 0.5) {
       return NextResponse.redirect(new URL('/dashboard', request.url))
